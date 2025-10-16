8fba3c6a09f2074425653dd6a0d8ea5f1506cac8
Samir Haziel <samir.alfonso@tecsup.edu.pe>
2025-10-15 22:07:19 -0500

    serializers: Cliente y Pedido (+ cliente_nombre personalizado)

---
+++ orders/serializers.py
@@ -0,0 +1,25 @@
+from rest_framework import serializers
+from .models import Cliente, Pedido
+
+class ClienteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Cliente
+        fields = ["id", "nombre", "direccion", "email", "telefono"]
+
+
+class PedidoSerializer(serializers.ModelSerializer):
+    cliente = serializers.PrimaryKeyRelatedField(queryset=Cliente.objects.all())
+    cliente_detalle = ClienteSerializer(source="cliente", read_only=True)
+    cliente_nombre = serializers.CharField(source="cliente.nombre", read_only=True)
+
+    class Meta:
+        model = Pedido
+        fields = [
+            "id",
+            "fecha",
+            "monto_total",
+            "estado",
+            "cliente",          # se envía el id del cliente
+            "cliente_detalle",  # muestra todo el cliente relacionado
+            "cliente_nombre",   # campo personalizado (+1 punto extra)
+        ]
